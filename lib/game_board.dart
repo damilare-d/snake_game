4d45882a96876aa618aff40548f7cc475a9969b7
Akinwale Damilare <damilareakinwale0@gmail.com>
2023-04-04 15:51:54 +0100

    added the food and obstacles

--- lib/game_board.dart
+++ lib/game_board.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -18,6 +20,30 @@ class _GameBoardState extends State<GameBoard> {
     Position(column: 0, row: 1),
     Position(column: 0, row: 2),
   ];
+  Position food = Position(column: 5, row: 5);
+  Position obstacle = Position(column: 6, row: 6);
+
+  @override
+  void initState() {
+    super.initState();
+    generateFood();
+    generateObstacle();
+  }
+
+  void generateFood() {
+    // the function is to generate a random position for food
+    int foodrow = Random().nextInt(widget.rows);
+    int foodcolumn = Random().nextInt(widget.columns);
+    food = Position(column: foodcolumn, row: foodrow);
+  }
+
+  void generateObstacle() {
+    //the function is to generate a random position for the obstacle
+    int obstaclerow = Random().nextInt(widget.rows);
+    int obstaclecolumn = Random().nextInt(widget.columns);
+
+    obstacle = Position(column: obstaclecolumn, row: obstaclerow);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,15 +53,30 @@ class _GameBoardState extends State<GameBoard> {
         crossAxisCount: widget.columns,
       ),
       itemBuilder: (BuildContext context, int index) {
-        // Check if the current index is part of the snake's body
+        // to check if the current index is part of the snake's body
         bool isSnake =
             snake.any((pos) => pos.row * widget.columns + pos.column == index);
 
-        // Return a Snake widget or an empty Container
-        return isSnake
-            ? Snake(snake: snake)
-            : Container(
-                color: Colors.grey[300], margin: const EdgeInsets.all(2));
+        // to check if the current index is the food
+        bool isFood = food.row * widget.columns + food!.column == index;
+
+        // to check if the current index is the food
+        bool isObstacle =
+            obstacle!.row * widget.columns + obstacle!.column == index;
+
+        // to return a Snake widget or an empty Container
+        if (isSnake) {
+          return Snake(snake: snake);
+        } else if (isFood) {
+          return const Food();
+        } else if (isObstacle) {
+          return const Obstacle();
+        } else {
+          return Container(
+            color: Colors.grey[300],
+            margin: const EdgeInsets.all(2),
+          );
+        }
       },
     );
   }
@@ -49,6 +90,29 @@ class Snake extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(color: Colors.green, margin: const EdgeInsets.all(2));
+  }
+}
+
+class Food extends StatelessWidget {
+  const Food({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      margin: const EdgeInsets.all(2),
+    );
+  }
+}
+
+class Obstacle extends StatelessWidget {
+  const Obstacle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      margin: const EdgeInsets.all(2),
+    );
   }
 }
 
